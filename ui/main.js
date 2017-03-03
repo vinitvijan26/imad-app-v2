18c3be25c87f149ba5e4791d382c8f9483b87fff
Vinit Vijan <vinit26virgo@gmail.com>
2017-03-03 16:28:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+var img = document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft = '100px';
+};
