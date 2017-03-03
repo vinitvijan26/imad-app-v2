b0fb0e393700817b237dfe3cf2664bd04e93a72c
Vinit Vijan <vinit26virgo@gmail.com>
2017-03-03 17:08:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,12 @@ element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
 
-var marginLeft = '0';
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick=function(){
-    var interval = setInterval(moveRight,1000);
+    var interval = setInterval(moveRight,100);
     //img.style.marginLeft = '100px';
 };
