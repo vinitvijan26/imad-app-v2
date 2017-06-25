492e1fcf6b77019d6daaad4e000ded071ba647af
Vinit Vijan <vinit26virgo@gmail.com>
2017-06-26 02:07:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -89,13 +89,23 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 var counter = 0;
 app.get('/counter',function(req,res){
    counter = counter + 1;
    res.send(counter.toString());
 });
 
-app.get('/:articleName', function(req, res){
+var names = [];
+app.get('/submit-name',function(req,res){       //'/submit-name?name=xxxxx'
+        var name = req.query.name;
+    //var name = req.params.name;     //done when the link is '/submit-name/:name'
+    names.push(name);
+    //convert array into string using JSON - JavaScript Object Notation
+    res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName',function(req, res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
@@ -104,14 +114,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/me.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'me.png'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
