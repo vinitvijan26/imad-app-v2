d99db9bbfe2858f4e4566894df51953e0e520af2
Vinit Vijan <vinit26virgo@gmail.com>
2017-08-12 22:14:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var app = express();
-app.use(morgan('combined'));
+app.use(morgan('combined'));        //express app
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
-var articles = {
-    'article-one' : {
-    title: 'Article-One | Vinit Vijan',
-    heading: 'Article-One',
-    date: '1st March 2017',
-    content: `  <p>
-                    Content 1
-                </p>
-                <p>
-                    Content 1
-                </p>
-                <p>
-                    Content 1
-                </p>`
-},
-    'article-two' : {
-    title: 'Article-Two | Vinit Vijan',
-    heading: 'Article-Two',
-    date: '2nd March 2017',
-    content: `  <p>
-                    Content 2
-                </p>
-                <p>
-                    Content 2
-                </p>
-                <p>
-                    Content 2
-                </p>`
-},
-    'article-three' : {
-    title: 'Article-Three | Vinit Vijan',
-    heading: 'Article-Three',
-    date: '3rd March 2017',
-    content: `  <p>
-                    Content 3
-                </p>
-                <p>
-                    Content 3
-                </p>
-                <p>
-                    Content 3
-                </p>`
-}
+require('dotenv').config({encoding: 'base64'});
+
+var config = {
+  port: '5432',
+  host: 'db.imad.hasura-app.io',
+  password: 'db-vinitvijan26-8534',
+  //password: process.env.DB_PASS,
+  user: 'vinitvijan26',
+  database: 'vinitvijan26'
 };
+
+// create the pool somewhere globally so its lifetime
+// lasts for as long as your app is running
+var pool = new Pool(config);
 
 function createTemplate(data){
     var title = data.title;
@@ -74,7 +53,7 @@ function createTemplate(data){
                     ${heading}
                 </h3>
                 <div>
-                    ${date}
+                    ${date.toDateString()}
                 </div>
                 <div>
                     ${content}
@@ -87,6 +66,87 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input);
+    res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
+});
+
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
 
 
@@ -105,9 +165,36 @@ app.get('/submit-name',function(req,res){       //'/submit-name?name=xxxxx'
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/test-db',function(req,res){
+    pool.query("SELECT * FROM article", function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            if(result.rows.length === 0){
+                res.status(404).send('Article not found');
+            } else {
+                res.send(JSON.stringify(result.rows));
+            }
+        }
+    });
+});
+
+app.get('/articles/:articleName',function(req, res){
+    //make a select request
+    //return a response with the results
+    // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -124,6 +211,10 @@ app.get('/ui/me.png', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/favicon.ico', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'favicon.png'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
